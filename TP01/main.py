95c892e7267aecfc6f14d18e3a3f5adfa4c8fc71
Bernardo Bartholomeu <bebartholomeu@hotmail.com>
2024-10-04 15:02:21 -0300

    Arrumando o código

--- TP01/main.py
+++ TP01/main.py
@@ -1,55 +1,46 @@
 from grafo import Grafo
 
 def main():
-    print("Escolha um tamanho de grafo:")
+    print("Escolha o tamanho do grafo:")
     print("1. 10 vértices")
     print("2. 100 vértices")
     print("3. 1000 vértices")
     print("4. 100000 vértices")
     
-    escolha_tamanho = input("Digite o número de vértices do grafo: ")
+    escolha_tamanho = input("Digite o número correspondente ao tamanho: ")
 
-    if escolha_tamanho == "1":
-        tamanho = 10
-    elif escolha_tamanho == "2":
-        tamanho = 100
-    elif escolha_tamanho == "3":
-        tamanho = 1000
-    elif escolha_tamanho == "4":
-        tamanho = 100000
-    else:
-        print("Escolha inválida.")
-        return
+    match escolha_tamanho:
+        case "1":
+            tamanho = 10
+        case "2":
+            tamanho = 100
+        case "3":
+            tamanho = 1000
+        case "4":
+            tamanho = 100000
+        case _:
+            print("Escolha inválida.")
+            return
+    g = Grafo(tamanho)
 
     print("Escolha o tipo de grafo:")
     print("1. Gerar grafo aleatório")
     print("2. Gerar árvore geradora mínima")
-    
     escolha_tipo = input("Digite o número do tipo de grafo: ")
 
-    g = Grafo(tamanho)
+    match escolha_tipo:
+        case "1":
+            g.gerar_grafo_aleatorio(tamanho)
+        case "2":
+            g.gerar_arvore_minima(tamanho)
+        case _:
+            print("Escolha inválida.")
+            return
 
-    if escolha_tipo == "1":
-        g.gerar_grafo_aleatorio(tamanho)
-    elif escolha_tipo == "2":
-        g.gerar_arvore_minima(tamanho)
-    else:
-        print("Escolha inválida.")
-        return
+    print("O grafo contém ciclos." if g.verificar_ciclos() else "O grafo não contém ciclos.")
 
-    if g.verificar_ciclos():
-        print("O grafo contém ciclos.")
-    else:
-        print("O grafo não contém ciclos.")
-    
     articulacoes = g.encontrar_vertices_articulacao()
-    if articulacoes:
-        print(f"Articulações encontradas: {articulacoes}")
-    else:
-        print("Não foram encontradas articulações.")
-
-    componentes = g.algoritmo_tarjan()
-    print("Componentes Fortemente Conectados:", componentes)
+    print(f"Articulações encontradas: {articulacoes}" if articulacoes else "Não foram encontradas articulações.")
 
 if __name__ == "__main__":
     main()
